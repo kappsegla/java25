c94790e1654f8354491982b4ad7d231cd1718d58
Martin Blomberg <martin.blomberg@outlook.com>
2025-09-02 13:22:05 +0200

    Read a number and divide 10 byt the number

--- src/main/java/org/fungover/Main.java
+++ src/main/java/org/fungover/Main.java
@@ -1,44 +1,24 @@
 package org.fungover;
 
-import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
+    //My first Java program 🌶️🌶️
     public static void main(String[] args) {
-        int age = 20;
-        byte catCount = -128;
-        var catCount2 = 1L;
-        String name = "Martin";
-        String multiLineText = """
-                Line 1
-                Line 2
-                """;
-        //Here we add 10 to the string before printing it
-        //Another line of comment
-        System.out.println("Hello World!" + 10);
-        /*
-        Multiple comments about adding 10 to age
-         */
-        System.out.println(age + 10);
-        System.out.println(30);
-        System.out.printf("Hello %s! Your age is %d and you have %d cats.%n", name, age, catCount);
-        System.out.println(Integer.toBinaryString(catCount));
-        System.out.println(Integer.toBinaryString(-128));
-
-
-        double weight = 5.0;
-        float weight2 = 5.0f;
-        BigDecimal bigDecimal = new BigDecimal("5.1");
-        BigInteger bigInteger = new BigInteger("5");
+        int x = 10;
+        String input = System.console().readLine("Enter a number: ");
+        int divider = Integer.parseInt(input);
+        try {
+            int y = x / divider;
+            System.out.println(y);
+        } catch (ArithmeticException e) {
+            System.out.println("You can't divide by zero!");
+        }
     }
 
     public static void test() {
 
     }
 
-    public static void record(){
+    public static void record() {
 
     }
 }
