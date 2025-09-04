3e80e082683dfd89f59d79326523bf931351780e
Martin Blomberg <martin.blomberg@outlook.com>
2025-09-04 09:44:41 +0200

    Exercise2

---
+++ src/main/java/org/fungover/exercises/week1/Exercise2.java
@@ -0,0 +1,13 @@
+package org.fungover.exercises.week1;
+
+public class Exercise2 {
+    public static void main(String[] args) {
+        int age = 25;
+        float weight = 100.f;
+        String initial = "EL";
+
+        System.out.println(age);
+        System.out.println(weight);
+        System.out.println(initial);
+    }
+}
