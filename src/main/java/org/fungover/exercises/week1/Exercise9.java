dde0e9d1f5a49e02df439b4b6da0ea3714e8a456
Martin Blomberg <martin.blomberg@outlook.com>
2025-09-04 13:34:19 +0200

    Exercise9 several options

--- src/main/java/org/fungover/exercises/week1/Exercise9.java
+++ src/main/java/org/fungover/exercises/week1/Exercise9.java
@@ -9,5 +9,29 @@ public class Exercise9 {
             case 'C', 'D' -> System.out.println("Godkänt.");
             default -> System.out.println("Underkänt.");
         }
+        switch (grade) {
+            case 'A':
+                System.out.println("Utmärkt!");
+                break;
+            case 'B':
+                System.out.println("Bra jobbat!");
+                break;
+            case 'C':
+            case 'D':
+                System.out.println("Godkänt.");
+                break;
+            default:
+                System.out.println("Underkänt.");
+                break;
+        }
+        if (grade == 'A') {
+            System.out.println("Utmärkt!");
+        } else if (grade == 'B') {
+            System.out.println("Bra jobbat!");
+        } else if (grade == 'C' || grade == 'D') {
+            System.out.println("Godkänt.");
+        } else {
+            System.out.println("Underkänt.");
+        }
     }
 }
