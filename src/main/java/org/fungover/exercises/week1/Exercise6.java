0035d9326b17a02a4f4fb0234c712acf17bb8e54
Martin Blomberg <martin.blomberg@outlook.com>
2025-09-04 11:20:39 +0200

    Exercise6b

--- src/main/java/org/fungover/exercises/week1/Exercise6.java
+++ src/main/java/org/fungover/exercises/week1/Exercise6.java
@@ -10,11 +10,14 @@ public class Exercise6 {
         String input = System.console().readLine("Ge mig ditt första tal ");
         String input2 = System.console().readLine("Ge mig ditt andra tal ");
         try {
+//            if( Character.isDigit(input.charAt(0)))
+//                System.out.println("input contains number");
             int tal1 = Integer.parseInt(input);
             int tal2 = Integer.parseInt(input2);
             double meanValue = (tal1 + tal2) / 2.0;
             System.out.println("Medelvärdet är " + meanValue);
 
+            //Alternative use Math.max(tal1,tal2)
             if (tal1 > tal2) {
                 System.out.println("Det Största talet är " + tal1);
             } else {
