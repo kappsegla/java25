b0ee9c4bf490e7aa53c831551c2244c7cf4bb76d
Martin Blomberg <martin.blomberg@outlook.com>
2025-09-08 13:30:43 +0200

    Find the biggest number

--- src/main/java/org/fungover/week3/LoopsAndArrays.java
+++ src/main/java/org/fungover/week3/LoopsAndArrays.java
@@ -7,17 +7,28 @@ public class LoopsAndArrays {
     public static void main(String[] args) {
         //Ask the user for values
         //Print the values
+        //Biggest number in the array
+        //Mean value of the numbers
+        //Biggest number we can make by adding two numbers from the array
+        //Sorting
+        //Extend array size?
+
         int numberCount = Integer.parseInt(System.console().readLine("How many numbers do you have "));
         int[] values = new int[numberCount];
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < numberCount; i++) {
             int value = Integer.parseInt(System.console().readLine("Enter a value "));
             values[i] = value;
         }
 
+        //Find the biggest number in the array
+        int maxValue = values[0];
+        for (int i = 1; i < values.length; i++) {
+            maxValue = Math.max(maxValue, values[i]);
+        }
+        System.out.println("The biggest number: " + maxValue);
+
+
         System.out.println(Arrays.toString(values));
-        //System.out.println("The values are " + value1 + ", " + value2 + ", " + value3);
-
-
     }
 }
