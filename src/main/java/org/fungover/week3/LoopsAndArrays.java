a00d88d408da8d2b0302fb3bb50839335eb4ff91
Martin Blomberg <martin.blomberg@outlook.com>
2025-09-08 14:22:03 +0200

    Extract methods

--- src/main/java/org/fungover/week3/LoopsAndArrays.java
+++ src/main/java/org/fungover/week3/LoopsAndArrays.java
@@ -22,13 +22,30 @@ public class LoopsAndArrays {
         }
 
         //Find the biggest number in the array
+        int maxValue = maxValue(values);
+        System.out.println("The biggest number: " + maxValue);
+        //Mean value of the numbers
+        double result = average(values);
+        System.out.println("Average value: " + result);
+        
+        System.out.println(Arrays.toString(values));
+    }
+    
+    public static int maxValue(int[] values){
         int maxValue = values[0];
         for (int i = 1; i < values.length; i++) {
             maxValue = Math.max(maxValue, values[i]);
         }
-        System.out.println("The biggest number: " + maxValue);
-
-
-        System.out.println(Arrays.toString(values));
+        return maxValue;
     }
+
+    public static double average(int[] values){
+        int sum = 0;
+        for (int i = 0; i < values.length; i++) {
+            sum = sum + values[i];
+        }
+        double result = ((double) sum) / values.length;
+        return result;
+    }
+    
 }
