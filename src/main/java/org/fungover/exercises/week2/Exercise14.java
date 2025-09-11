11fa33f5c550e63462025439147e65f81e5c9e12
Martin Blomberg <martin.blomberg@outlook.com>
2025-09-11 14:20:19 +0200

    Exercise14

---
+++ src/main/java/org/fungover/exercises/week2/Exercise14.java
@@ -0,0 +1,27 @@
+package org.fungover.exercises.week2;
+
+import java.util.Arrays;
+
+import static java.lang.System.console;
+
+public class Exercise14 {
+    public static void main(String[] args) {
+        //Läs in två ord i String
+        String word1 = console().readLine("Enter a word: ").toLowerCase().trim();
+        String word2 = console().readLine("Another word: ").toLowerCase().trim();
+
+        //Testa för anagram?
+        if (word1.length() != word2.length())
+            System.out.println("Can't be an anagram");
+
+        //Sortera bokstäverna i orden och jämför
+        var charArrayForWord1 = word1.toCharArray();
+        var charArrayForWord2 = word2.toCharArray();
+        Arrays.sort(charArrayForWord1);
+        Arrays.sort(charArrayForWord2);
+        if (Arrays.compare(charArrayForWord1, charArrayForWord2) == 0)
+            System.out.println("The words " + word1 + " and " + word2 + " are anagrams");
+
+
+    }
+}
