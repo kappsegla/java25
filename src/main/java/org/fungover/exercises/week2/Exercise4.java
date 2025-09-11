c372d28d47f9264177a06e18dfcf5128e73f0279
Martin Blomberg <martin.blomberg@outlook.com>
2025-09-11 09:46:10 +0200

    Exercise4 week2

--- src/main/java/org/fungover/exercises/week2/Exercise4.java
+++ src/main/java/org/fungover/exercises/week2/Exercise4.java
@@ -1,5 +1,7 @@
 package org.fungover.exercises.week2;
 
+import java.util.Scanner;
+
 public class Exercise4 {
     public static void main(String[] args) {
         boolean stop = false;
@@ -13,5 +15,22 @@ public class Exercise4 {
                 System.out.println(output);
             }
         }
+        //Adding strings together wastes memory a bit since strings are immutable.
+        //A better approach is to use a StringBuilder which is a mutable string.
+        Scanner sc = new Scanner(System.in);
+        StringBuilder result = new StringBuilder();
+        while (true) {
+            System.out.println(" Please enter your word: ");
+            String input = sc.nextLine();
+            result.append(input);
+            if (!result.isEmpty()) {
+                result.append(" ");
+            }
+            if (input.isEmpty() || input.equalsIgnoreCase(".")) {
+                break;
+            }
+        }
+        System.out.println(" your words: " + result.toString());
+
     }
 }
