1b5b632c02c3f58cdfca69b755939c670b20fd5f
Martin Blomberg <martin.blomberg@outlook.com>
2025-09-11 13:37:35 +0200

    Palindrom ignoring space

--- src/main/java/org/fungover/exercises/week2/Exercise13.java
+++ src/main/java/org/fungover/exercises/week2/Exercise13.java
@@ -2,10 +2,14 @@ package org.fungover.exercises.week2;
 
 import static java.lang.System.console;
 
-public class Exercise12 {
+public class Exercise13 {
     public static void main(String[] args) {
         //Skapa string med texten radar
         String palindrom = console().readLine("Enter a word: ");
+        //Ta bort alla mellanslag...
+        palindrom = palindrom
+                .toLowerCase()
+                .replace(" ", "");
 
         //Skapa en ny string in reverse med StringBuilder
         String reversed = new StringBuilder().append(palindrom).reverse().toString();
@@ -19,6 +23,12 @@ public class Exercise12 {
         System.out.println("Is palindrom: " + isPalindrome(palindrom));
     }
 
+    /**
+     * This method checks a string for palindrom compliance
+     *
+     * @param word Input string to check
+     * @return true if word is palindrome otherwise false
+     */
     private static boolean isPalindrome(String word) {
         for (int i = 0; i < word.length() / 2; i++)
             if (word.charAt(i) != word.charAt(word.length() - 1 - i))
