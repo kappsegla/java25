2870a8bd3834238417964ee61ee409135764c32f
Martin Blomberg <martin.blomberg@outlook.com>
2025-09-11 09:06:56 +0200

    Exercise1 week2

---
+++ src/main/java/org/fungover/exercises/week2/Exercise1.java
@@ -0,0 +1,10 @@
+package org.fungover.exercises.week2;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 16; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
