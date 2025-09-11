9e9ecac276151dd8f5836bcfda8bf72f1d16ac9a
Martin Blomberg <martin.blomberg@outlook.com>
2025-09-11 13:23:05 +0200

    Palindrom

---
+++ src/main/java/org/fungover/exercises/week2/Exercise12.java
@@ -0,0 +1,28 @@
+package org.fungover.exercises.week2;
+
+import static java.lang.System.console;
+
+public class Exercise12 {
+    public static void main(String[] args) {
+        //Skapa string med texten anna
+        String palindrom = console().readLine("Enter a word: ");
+
+        //Skapa en ny string in reverse med StringBuilder
+        String reversed = new StringBuilder().append(palindrom).reverse().toString();
+
+        //Jämför original strängen med reverse strängen equals..
+        if (palindrom.equals(reversed))
+            System.out.println("This is a palindrom");
+        else
+            System.out.println("Not a palindrom");
+
+        System.out.println("Is palindrom: " + isPalindrome(palindrom));
+    }
+
+    private static boolean isPalindrome(String word) {
+        for (int i = 0; i < word.length() / 2; i++)
+            if (word.charAt(i) != word.charAt(word.length() - 1 - i))
+                return false;
+        return true;
+    }
+}
