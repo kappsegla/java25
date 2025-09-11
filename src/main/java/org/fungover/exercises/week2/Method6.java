db544933f531e4468fe21b934eca0a1b13590455
Martin Blomberg <martin.blomberg@outlook.com>
2025-09-11 15:45:23 +0200

    Method6

--- src/main/java/org/fungover/exercises/week2/Method6.java
+++ src/main/java/org/fungover/exercises/week2/Method6.java
@@ -16,12 +16,12 @@ public class Method6 {
     }
 
     public static boolean isPrime(int number) {
-        if (number == 2)
+        if (number == 2 || number == 3)
             return true;
         if (number % 2 == 0)
             return false;
 
-        for (int i = 2; i <= Math.sqrt(number); i++)
+        for (int i = 5; i <= Math.sqrt(number); i++)
             if (number % i == 0)
                 return false;
         return true;
