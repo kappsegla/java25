1d36a201a92f9404ac26e8f8b146d92df1dc3af9
Martin Blomberg <martin.blomberg@outlook.com>
2025-09-16 13:43:46 +0200

    MapMarkers

---
+++ src/main/java/org/fungover/oop/MapMarkers.java
@@ -0,0 +1,16 @@
+package org.fungover.oop;
+
+public class MapMarkers {
+
+    public static void main(String[] args) {
+
+        Point point = new Point();
+        Point point1 = new Point(3.0, 4.0);
+
+        System.out.println(point.distanceTo(point1));
+
+
+    }
+
+
+}
