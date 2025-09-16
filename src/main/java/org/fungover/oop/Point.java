55d6bc96290037a65afad06ec594d804534df9ec
Martin Blomberg <martin.blomberg@outlook.com>
2025-09-16 13:19:25 +0200

    Create Point

---
+++ src/main/java/org/fungover/oop/Point.java
@@ -0,0 +1,58 @@
+package org.fungover.oop;
+
+import java.util.Objects;
+
+public class Point {
+    private double x;
+    private double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point(Point other) {
+        this.x = other.x;
+        this.y = other.y;
+    }
+
+    public Point() {
+        this.x = 0.0;
+        this.y = 0.0;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Point point)) return false;
+        return Double.compare(x, point.x) == 0 && Double.compare(y, point.y) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
