b42912aa429872322827af080005bea022a59af8
Martin Blomberg <martin.blomberg@outlook.com>
2025-09-18 09:46:06 +0200

    Add Car

---
+++ src/main/java/org/fungover/exercises/week3/Car.java
@@ -0,0 +1,32 @@
+package org.fungover.exercises.week3;
+
+public class Car {
+    private String model;
+    private int price;
+    private String color;
+
+
+    public String getModel() {
+        return this.model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
