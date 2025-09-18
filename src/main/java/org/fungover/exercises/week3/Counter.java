87728460790e36fbb615831e1f67a06e45ee15ad
Martin Blomberg <martin.blomberg@outlook.com>
2025-09-18 10:42:36 +0200

    Counter counter

--- src/main/java/org/fungover/exercises/week3/Counter.java
+++ src/main/java/org/fungover/exercises/week3/Counter.java
@@ -3,6 +3,12 @@ package org.fungover.exercises.week3;
 public class Counter {
     private int counter;
 
+    private static int objectCount;
+
+    public Counter() {
+        objectCount++;
+    }
+
     public void increase() {
         counter++;
     }
@@ -15,10 +21,22 @@ public class Counter {
         return counter;
     }
 
+    public int getObjectCount() {
+        return objectCount;
+    }
+
+    public void print() {
+        System.out.println("Count is: " + counter);
+        System.out.println("Object count: " + objectCount);
+    }
+
     static void main() {
         Counter counter = new Counter();
+        Counter counter1 = new Counter();
+        Counter counter2 = new Counter();
         doSomeCounting(counter);
         System.out.println(counter.value());
+        counter2.print();
     }
 
     static void doSomeCounting(Counter counter) {
