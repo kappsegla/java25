37652c5d74150295b585a413a37a3372e50f5eb7
Martin Blomberg <martin.blomberg@outlook.com>
2025-09-18 13:15:08 +0200

    Uppgift 1 och 2

---
+++ src/main/java/org/fungover/exercises/week3distance/Animal.java
@@ -0,0 +1,13 @@
+package org.fungover.exercises.week3distance;
+
+public class Animal {
+    public String species;
+    public int age;
+    public String name;
+
+    public void printToConsole() {
+        System.out.println("Name: " + name + ", Age: " + age + ", Species: " + species);
+    }
+
+
+}
