26597d73f08f9df5d399de62cab1d118103de074
Martin Blomberg <martin.blomberg@outlook.com>
2025-09-18 13:31:40 +0200

    Uppgift 3

--- src/main/java/org/fungover/exercises/week3distance/Animal.java
+++ src/main/java/org/fungover/exercises/week3distance/Animal.java
@@ -5,9 +5,21 @@ public class Animal {
     public int age;
     public String name;
 
+    public Animal(String name, int age, String species) {
+        this.name = name;
+        this.age = age;
+        this.species = species;
+    }
+
     public void printToConsole() {
         System.out.println("Name: " + name + ", Age: " + age + ", Species: " + species);
     }
 
+    static void main() {
+        Animal animal1 = new Animal("Bengt", 7, "Gazelle");
+        Animal animal2 = new Animal("Gösta", 2, "Giraffe");
+        animal1.printToConsole();
+        animal2.printToConsole();
+    }
 
 }
