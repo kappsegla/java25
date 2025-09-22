a33651170d64d16482c4cb8f9192215458cda3ea
Martin Blomberg <martin.blomberg@outlook.com>
2025-09-22 13:10:27 +0200

    Extract methods

--- src/main/java/org/fungover/oop2/RockPaperScissors.java
+++ src/main/java/org/fungover/oop2/RockPaperScissors.java
@@ -7,26 +7,17 @@ public class RockPaperScissors {
 
     static void main() {
         //Input av human choice
-        String input = IO.readln();
-        int playerChoice =
-                switch (input.trim().toLowerCase()) {
-                    case "rock", "r" -> ROCK;
-                    case "paper", "p" -> PAPER;
-                    case "scissors", "s" -> SCISSORS;
-                    default -> throw new IllegalArgumentException();
-                };
-
+        int playerChoice = getPlayerChoice();
         //Datorns val
-        int computerChoice;
-        double rand = Math.random();
-        if (rand < 0.33) {
-            computerChoice = ROCK;
-        } else if (rand < 0.66) {
-            computerChoice = PAPER;
-        } else
-            computerChoice = SCISSORS;
-
+        int computerChoice = getComputerChoice();
         //Kolla vinnare
+        checkWinner(computerChoice, playerChoice);
+        //Todo: Ge vinnaren poäng
+        //Todo: Flera rundor
+        //Todo: Snygga utskrifter
+    }
+
+    private static void checkWinner(int computerChoice, int playerChoice) {
         if (computerChoice == playerChoice) {
             //Draw
         } else if (computerChoice == ROCK && playerChoice == SCISSORS) {
@@ -38,9 +29,29 @@ public class RockPaperScissors {
         } else {
             //Player wins
         }
-        //Ge vinnaren poäng
-        //Flera rundor
+    }
 
+    private static int getComputerChoice() {
+        int computerChoice;
+        double rand = Math.random();
+        if (rand < 0.33) {
+            computerChoice = ROCK;
+        } else if (rand < 0.66) {
+            computerChoice = PAPER;
+        } else
+            computerChoice = SCISSORS;
+        return computerChoice;
+    }
 
+    private static int getPlayerChoice() {
+        String input = IO.readln("Your choice (r)ock,(p)aper,(s)cissors: ");
+        int playerChoice =
+                switch (input.trim().toLowerCase()) {
+                    case "rock", "r" -> ROCK;
+                    case "paper", "p" -> PAPER;
+                    case "scissors", "s" -> SCISSORS;
+                    default -> throw new IllegalArgumentException();
+                };
+        return playerChoice;
     }
 }
