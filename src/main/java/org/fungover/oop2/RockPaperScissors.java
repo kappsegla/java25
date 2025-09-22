0cae3512658c819317eef39232f82f3e776ea916
Martin Blomberg <martin.blomberg@outlook.com>
2025-09-22 14:06:57 +0200

    All works now

--- src/main/java/org/fungover/oop2/RockPaperScissors.java
+++ src/main/java/org/fungover/oop2/RockPaperScissors.java
@@ -4,31 +4,59 @@ public class RockPaperScissors {
     public static final int ROCK = 1;
     public static final int PAPER = 2;
     public static final int SCISSORS = 3;
+    public static final int MAX_SCORE = 3;
+
+    private static final ScoreBoard scoreBoard = new ScoreBoard();
 
     static void main() {
-        //Input av human choice
-        int playerChoice = getPlayerChoice();
-        //Datorns val
-        int computerChoice = getComputerChoice();
-        //Kolla vinnare
-        checkWinner(computerChoice, playerChoice);
-        //Todo: Ge vinnaren poäng
-        //Todo: Flera rundor
-        //Todo: Snygga utskrifter
+        while (scoreBoard.player1Score() < MAX_SCORE &&
+                scoreBoard.player2Score() < MAX_SCORE) {
+            //Input av human choice
+            int playerChoice;
+            try {
+                playerChoice = getPlayerChoice();
+            } catch (IllegalArgumentException e) {
+                continue;
+            }
+            //Datorns val
+            int computerChoice = getComputerChoice();
+            //Kolla vinnare
+            checkWinner(computerChoice, playerChoice);
+        }
     }
 
     private static void checkWinner(int computerChoice, int playerChoice) {
+        printChoices(computerChoice, playerChoice);
         if (computerChoice == playerChoice) {
             //Draw
+            printWinner(" It's a draw.");
         } else if (computerChoice == ROCK && playerChoice == SCISSORS) {
             //Computer wins
+            scoreBoard.increasePlayer2Score();
+            printWinner(" Computer wins.");
         } else if (computerChoice == PAPER && playerChoice == ROCK) {
             //Computer wins
+            scoreBoard.increasePlayer2Score();
+            printWinner(" Computer wins.");
         } else if (computerChoice == SCISSORS && playerChoice == PAPER) {
             //Computer wins
+            scoreBoard.increasePlayer2Score();
+            printWinner(" Computer wins.");
         } else {
             //Player wins
+            scoreBoard.increasePlayer1Score();
+            printWinner(" Player wins.");
         }
+    }
+
+    private static void printChoices(int computerChoice, int playerChoice) {
+        System.out.print("Computer picks " + computerChoice + ", Player picks " + playerChoice);
+    }
+
+    private static void printWinner(String x) {
+        System.out.println(x);
+        System.out.println("Score: Computer " + scoreBoard.player2Score() + ", Player " + scoreBoard.player1Score());
+
     }
 
     private static int getComputerChoice() {
