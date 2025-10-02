1eb41c6c375a7372256a67172d9ee2e56e707290
Martin Blomberg <martin.blomberg@outlook.com>
2025-10-02 09:12:30 +0200

    Exercise 2 week 5

--- src/main/java/org/fungover/exercises/week5/Food.java
+++ src/main/java/org/fungover/exercises/week5/Food.java
@@ -2,9 +2,25 @@ package org.fungover.exercises.week5;
 
 public class Food {
 
+    public void checkExpired() {
+        System.out.println("Hasn't expired");
+    }
+
+    public void consume() {
+        System.out.println("Eating some food");
+    }
+
+
     static void main() {
         Food food = new Food();
         Milk milk = new Milk();
         Food food1 = new Milk();
+
+        food.consume();
+        food1.consume();
+        milk.consume();
+        food1.checkExpired();
+        milk.checkExpired();
+
     }
 }
--- src/main/java/org/fungover/exercises/week5/Milk.java
+++ src/main/java/org/fungover/exercises/week5/Milk.java
@@ -1,4 +1,9 @@
 package org.fungover.exercises.week5;
 
 public class Milk extends Food {
+
+    @Override
+    public void consume() {
+        System.out.println("Drinking my milk");
+    }
 }
