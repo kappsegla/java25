3dfd8c3ef13e3e462b037d3c0dd005e0bdecec4b
Martin Blomberg <martin.blomberg@outlook.com>
2025-10-07 12:57:29 +0200

    LinkedList get

--- src/main/java/org/fungover/oop4/LinkedList.java
+++ src/main/java/org/fungover/oop4/LinkedList.java
@@ -5,6 +5,11 @@ public class LinkedList {
     Node first = null;
     Node last = null;
 
+    /**
+     * Stores value in a new Node that is added to the end of the List
+     *
+     * @param value
+     */
     public void add(String value) {
         //First value added
         if (first == null) {
@@ -18,7 +23,7 @@ public class LinkedList {
             last.next = next;
             last = next;
             last.value = value;
-            //Alternative solution without keepin a last reference
+            //Alternative solution without keeping a last reference
             //        Node temp = first;
             //        while( temp.next != null)
             //            temp = temp.next;
@@ -27,14 +32,27 @@ public class LinkedList {
     }
 
     public String get(int index) {
-        return "";
+        int counter = 0;
+        Node temp = first;
+        if (temp == null)
+            throw new IndexOutOfBoundsException();
+        while (counter < index) {
+            if (temp == null)
+                throw new IndexOutOfBoundsException();
+            temp = temp.next;
+            counter++;
+        }
+        return temp.value;
     }
 
     static void main() {
         LinkedList list = new LinkedList();
+        //list.get(0);  //Fail because empty list
         list.add("Number 1");
         list.add("Number 2");
         list.add("Number 3");
+        System.out.println(list.get(1));
+        //list.get(4);  //Fail bacause too far
     }
 }
 
