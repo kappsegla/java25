19c18c0b8e874f3ea99b35a9190d5d7c7ce4cc8d
Martin Blomberg <martin.blomberg@outlook.com>
2025-10-07 14:26:23 +0200

    Generic method

--- src/main/java/org/fungover/oop4/Wildcards.java
+++ src/main/java/org/fungover/oop4/Wildcards.java
@@ -7,15 +7,26 @@ public class Wildcards {
 
     static void main() {
         List<String> strings = new ArrayList<>();
+        strings.add("First");
         List<Integer> integers = new ArrayList<>();
-
+        integers.add(1);
         printList(strings);
         printList(integers);
+        System.out.println(getFirst(strings));
+        System.out.println(getFirst(integers));
+        var s = getFirst(strings);
+        var i = getFirst(integers);
     }
 
     public static void printList(List<?> theList) {
         for (var e : theList)
             System.out.println(e);
     }
+
+    public static <T> T getFirst(List<T> theList) {
+        return theList.get(0);
+    }
+
+
 
 }
