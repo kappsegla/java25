7c0067e6466a2e7f05123a294c436f9547a9845e
Martin Blomberg <martin.blomberg@outlook.com>
2025-10-09 14:40:46 +0200

    Safe exercise 1

--- src/main/java/org/fungover/exercises/week6/generics/Safe.java
+++ src/main/java/org/fungover/exercises/week6/generics/Safe.java
@@ -1,4 +1,29 @@
 package org.fungover.exercises.week6.generics;
 
-public class Safe {
+public class Safe<T> {
+    private T object;
+
+    public void store(T object) {
+        if (object == null)
+            throw new IllegalArgumentException("This safe won't store empty things");
+        if (this.object != null)
+            throw new IllegalStateException("Safe is full. Can't store more stuff");
+        this.object = object;
+    }
+
+    public T retrieve() {
+        if (object == null)
+            throw new IllegalStateException("Safe is empty. Nothing to retrieve");
+        T temp = object;
+        object = null;
+        return temp;
+    }
+
+    static void main() {
+        Safe<String> safe = new Safe<>();
+        safe.store("Secret Stuff");
+        safe.store(null);
+        var stuff = safe.retrieve();
+        System.out.println(stuff);
+    }
 }
