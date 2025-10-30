1e27922bc30597c3b15cdaacffb629c78e61e619
Martin Blomberg <martin.blomberg@outlook.com>
2025-10-30 09:39:11 +0100

    Regexp 3

--- src/main/java/org/fungover/exercises/regexp/Regexp.java
+++ src/main/java/org/fungover/exercises/regexp/Regexp.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 
 public class Regexp {
     static void main() {
-        exercise2();
+        exercise3();
     }
 
     static void exercise1() {
@@ -19,13 +19,23 @@ public class Regexp {
     }
 
     static void exercise2() {
-//        String s = "kat|kon|kan";
-//        String s = "k(at|an|on)";
+        String s = "kat|kon|kan";
+//String s = "k(at|an|on)";
 //        String s = "ka[tn]|kon";
 //        String s = "k[ao][tn]";  //Matches kot also... not in the approved list of words
-        String s = "k(?!ot)[ao][tn]";  //Negative lookahead (?! ) Don't include in the matchresult group
+//        String s = "k(?!ot)[ao][tn]";  //Negative lookahead (?! ) Don't include in the matchresult group
         Pattern p = Pattern.compile(s);
-        Matcher m = p.matcher("kot min katt har tre konkatenerade kanter");
+        Matcher m = p.matcher("min katt har tre konkatenerade kanter");
+        m.results()
+                .peek(mr -> System.out.println(">" + mr.start() + ":" + mr.end()))  //Prints start and end pos of match
+                .map(MatchResult::group)  //The matched sequence
+                .forEach(System.out::println);
+    }
+
+    static void exercise3() {
+        String s = "a[a-zåäö]";
+        Pattern p = Pattern.compile(s);
+        Matcher m = p.matcher("min katt har tre konkatenerade kanter");
         m.results()
                 .peek(mr -> System.out.println(">" + mr.start() + ":" + mr.end()))  //Prints start and end pos of match
                 .map(MatchResult::group)  //The matched sequence
