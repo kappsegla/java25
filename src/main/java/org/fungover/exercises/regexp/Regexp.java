06ceaf31f5106c21f6e15a7075fd0705e4444b41
Martin Blomberg <martin.blomberg@outlook.com>
2025-10-30 09:32:04 +0100

    Regexp 1 and 2

---
+++ src/main/java/org/fungover/exercises/regexp/Regexp.java
@@ -0,0 +1,34 @@
+package org.fungover.exercises.regexp;
+
+import java.util.regex.MatchResult;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Regexp {
+    static void main() {
+        exercise2();
+    }
+
+    static void exercise1() {
+        String s = "kat";
+        Pattern p = Pattern.compile(s);
+        Matcher m = p.matcher("min katt har tre konkatenerade kanter");
+        System.out.println(m.find());
+//        System.out.println(m.results().findAny().isPresent());
+
+    }
+
+    static void exercise2() {
+//        String s = "kat|kon|kan";
+//        String s = "k(at|an|on)";
+//        String s = "ka[tn]|kon";
+//        String s = "k[ao][tn]";  //Matches kot also... not in the approved list of words
+        String s = "k(?!ot)[ao][tn]";  //Negative lookahead (?! ) Don't include in the matchresult group
+        Pattern p = Pattern.compile(s);
+        Matcher m = p.matcher("kot min katt har tre konkatenerade kanter");
+        m.results()
+                .peek(mr -> System.out.println(">" + mr.start() + ":" + mr.end()))  //Prints start and end pos of match
+                .map(MatchResult::group)  //The matched sequence
+                .forEach(System.out::println);
+    }
+}
